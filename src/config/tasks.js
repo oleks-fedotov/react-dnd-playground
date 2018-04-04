70a527e96f8b7eddab0069c3079eda77a07d8ff9
Oleksandr Fedotov <fedotov4815162342@gmail.com>
2018-04-04 22:23:16 +0200

    add more tasks

--- src/config/tasks.js
+++ src/config/tasks.js
@@ -13,6 +13,36 @@ export const tasks = [
     id: 3,
     title: 'Add drag n drop',
     status: 'todo'
+  },
+  {
+    id: 4,
+    title: 'Create draggable decorator',
+    status: 'todo'
+  },
+  {
+    id: 5,
+    title: 'Create droppable decorator',
+    status: 'todo'
+  },
+  {
+    id: 6,
+    title: 'Create hoverable decorator',
+    status: 'todo'
+  },
+  {
+    id: 7,
+    title: 'Apply decorators',
+    status: 'todo'
+  },
+  {
+    id: 8,
+    title: 'Fix bugs',
+    status: 'todo'
+  },
+  {
+    id: 9,
+    title: 'Drink beer',
+    status: 'todo'
   }
 ];
 
